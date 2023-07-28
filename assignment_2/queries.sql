0d73768eae983ae4f0193fae4037a6d2738e295a
m-sepehr <m-sepehr@hotmail.com>
2023-07-28 16:31:15 -0400

    added Part 2 d) and e)

--- assignment_2/queries.sql
+++ assignment_2/queries.sql
@@ -33,3 +33,28 @@ WHERE Sales.deliveryType = 'delivery' AND Member.memberID = Sales.clientID AND S
 city = 'Brossard' 
 AND dateOfSale BETWEEN '2023-06-01' AND '2023-06-30'
 ORDER BY Sales.saleID, firstName, lastName, weight;
+
+/*Part 2 d) 
+Give a report of the sales that have been picked up by the buyers in June 2023. 
+Details include sale ID, date of the sale, product description, price and weight of the product. 
+Results should be displayed in ascending order by saleID, then by price of the product.*/
+
+SELECT Sales.saleID, dateOfSale, productDescription, sellingPrice, weight
+FROM Sales, Products, SalesItems
+WHERE Sales.deliveryType = 'pickup' AND Sales.saleID = SalesItems.saleID AND SalesItems.productID = Products.productID 
+AND Sales.dateOfSale BETWEEN '2023-06-01' AND '2023-06-30'
+ORDER BY Sales.saleID, sellingPrice;
+
+/*Part 2 e)
+For every client who have made a purchase of at least 1000.00$ since the beginning of her/his membership, 
+give a report of the sales she/he has done since her/his membership. The report should include the client’s 
+first and last name, the date of the first sale she/he has made, the date of the last sale she/he has made, 
+the total amount of sales she/he has made. The results should be displayed in increasing order by total amount 
+of sales.*/
+
+SELECT firstName,lastName, MIN(dateOfSale) AS firstSale, MAX(dateOfSale) AS lastSale, SUM(amountOfSale) AS totalSales
+FROM Sales, Member
+WHERE Member.memberID = Sales.clientID
+GROUP BY firstName,lastName
+HAVING totalSales >= 1000
+ORDER BY totalSales;
